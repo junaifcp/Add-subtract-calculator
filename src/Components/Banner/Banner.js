3bc8432571c0d60707a2003fac2de32e8feebc4f
junaif <cpjunaif98@gmail.com>
2021-10-03 23:03:09 +0530

    done

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,36 @@
+
+import './Banner.css'
+import {useState} from 'react'
+
+function Banner() {
+
+    const [result,setResult]=useState(0) 
+    const [state,setState]=useState(0)
+    const [total,setTotal]=useState(result+state)
+     const addCount=()=>{
+         setTotal(Number(result)+Number(total))
+     }
+     const subCount=()=>{
+         setTotal(Number(total)-Number(result))
+        
+     }
+    return (
+        <div className='container'>
+            <div className="items">
+                <h3 className="item-head" style={{textAlign:'center'}}>Enter A Value To Add or Substract</h3><br/>
+                <input value={result} onChange={(e)=>setResult(e.target.value)} type='text' className='input' id='inputid'/>
+            </div><br/>
+            <div className="buttons">
+                <button id='btn1' className='button' onClick={addCount}>Add </button><br/>
+                <button id='btn2' className='button' onClick={subCount}>Subtract</button><br/><br/>
+            </div>
+            <div className="result">
+                <h1>RESULT</h1>
+                <h2 id='final'>{total}</h2>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Banner
